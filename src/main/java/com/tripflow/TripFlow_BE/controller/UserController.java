c78a28cd87d70d0050835c36ff02ee74a87e7a4b
LeeZEun <je990420@ajou.ac.kr>
2024-07-12 01:48:48 +0900

    [Feat] UserService return 타입 변환

--- src/main/java/com/tripflow/TripFlow_BE/controller/UserController.java
+++ src/main/java/com/tripflow/TripFlow_BE/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.tripflow.TripFlow_BE.controller;
 
-import com.tripflow.TripFlow_BE.dto.ResponseDataDto;
 import com.tripflow.TripFlow_BE.dto.join.JoinRequestDto;
+import com.tripflow.TripFlow_BE.dto.join.JoinResponseDto;
 import com.tripflow.TripFlow_BE.dto.login.LoginRequestDto;
+import com.tripflow.TripFlow_BE.dto.login.LoginResponseDto;
 import com.tripflow.TripFlow_BE.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +16,13 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/user/register")
-    public ResponseDataDto userJoin(@RequestBody JoinRequestDto reqDto)
+    public JoinResponseDto userJoin(@RequestBody JoinRequestDto reqDto)
     {
         return userService.join(reqDto);
     }
 
     @PostMapping("/user/login")
-    public ResponseDataDto userLogin(@RequestBody LoginRequestDto reqDto)
+    public LoginResponseDto userLogin(@RequestBody LoginRequestDto reqDto)
     {
         return userService.login(reqDto);
     }
--- src/main/java/com/tripflow/TripFlow_BE/service/UserService.java
+++ src/main/java/com/tripflow/TripFlow_BE/service/UserService.java
@@ -2,7 +2,6 @@ package com.tripflow.TripFlow_BE.service;
 
 import com.tripflow.TripFlow_BE.dto.join.JoinResponseDto;
 import com.tripflow.TripFlow_BE.dto.login.LoginResponseDto;
-import com.tripflow.TripFlow_BE.dto.ResponseDataDto;
 import com.tripflow.TripFlow_BE.dto.join.JoinRequestDto;
 import com.tripflow.TripFlow_BE.dto.login.LoginRequestDto;
 import com.tripflow.TripFlow_BE.entity.UserInfo;
@@ -23,54 +22,47 @@ public class UserService {
     private TokenProvider tokenProvider;
 
     //회원가입 확인
-    public ResponseDataDto<JoinResponseDto> join(JoinRequestDto users) {
+    public JoinResponseDto join(JoinRequestDto users) {
         Optional<UserInfo> optNickname = userRepository.findByNickname(users.getNickname());
         Optional<UserInfo> optUserid = userRepository.findByUserid(users.getUserid());
         Optional<UserInfo> optEmail = userRepository.findByEmail(users.getEmail());
         Optional<UserInfo> optPhonenumber = userRepository.findByPhonenumber(users.getPhonenumber());
 
-//        System.out.println(users.getUsername());
-
         if(optNickname.isEmpty() && optUserid.isEmpty() && optEmail.isEmpty() && optPhonenumber.isEmpty()){ //같은 게 없으면 (회원가입 성공)
             //초기 설정
             UserInfo userEntity = users.toEntity();
-//            System.out.println(userEntity.getUsername());
-//            System.out.println(userEntity.getNickname());
             userEntity.setCreatedtime(LocalDateTime.now()); //가입 일시
             userEntity.setRecessaccess(LocalDateTime.now()); //최근 접속
             userRepository.save(userEntity);
 
             JoinResponseDto responseDto = new JoinResponseDto(userEntity.getUsername(), userEntity.getNickname(), userEntity.getUserid(), userEntity.getEmail(), userEntity.getPhonenumber(), userEntity.getBirth(), userEntity.getCreatedtime(), userEntity.getRecessaccess());
-            return new ResponseDataDto("Signup Success", 200, responseDto);
+            return responseDto;
         }
         else { //회원가입 실패
-            return new ResponseDataDto("Signup Failed", 406, null);
+            return null;
         }
     }
 
     //로그인 확인
-    public ResponseDataDto<LoginResponseDto> login(LoginRequestDto users) {
+    public LoginResponseDto login(LoginRequestDto users) {
 
         Optional<UserInfo> optUserid = userRepository.findByUserid(users.getUserid());
-//        System.out.println(optUserid);
         if(optUserid.isPresent()){ //아이디가 있으면
             UserInfo userInfo = optUserid.get();
-//            System.out.println(userInfo);
             //데베에 저장된 비번과 입력받은 비번 비교
             if (userInfo.getPassword().equals(/*users.getPassword()*/optUserid.get().getPassword())) { //로그인 성공
                 final String jwttoken = tokenProvider.create(optUserid.get()); //토큰 생성
-//                System.out.println(jwttoken);
                 userInfo.setRecessaccess(LocalDateTime.now());
                 userRepository.save(userInfo);
                 LoginResponseDto responseDto = new LoginResponseDto(userInfo.getUsername(), userInfo.getNickname(), userInfo.getUserid(), userInfo.getEmail(), userInfo.getPhonenumber(), userInfo.getBirth(), userInfo.getCreatedtime(), userInfo.getRecessaccess(), jwttoken);
-                return new ResponseDataDto("Login Success", 200, responseDto);
+                return responseDto;
             }
             else {
-                return new ResponseDataDto("Info is wrong", 406, null);
+                return null;
             }
         }
         else {
-            return new ResponseDataDto("Id is not found", 406, null);
+            return null;
         }
     }
 }
