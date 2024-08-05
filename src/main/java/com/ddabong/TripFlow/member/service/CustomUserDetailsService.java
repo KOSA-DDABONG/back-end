95a251fed3a7ba2435cdf7b2096859c7125062d4
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-08-05 18:23:25 +0900

    [Refactor] CustomUserDetails 빈 주입 방식 변경

--- src/main/java/com/ddabong/TripFlow/member/service/CustomUserDetailsService.java
+++ src/main/java/com/ddabong/TripFlow/member/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.ddabong.TripFlow.member.service;
 import com.ddabong.TripFlow.member.dao.IMemberRepository;
 import com.ddabong.TripFlow.member.dto.CustomUserDetails;
 import com.ddabong.TripFlow.member.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,11 +11,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    private final IMemberRepository iMemberRepository;
 
-    public CustomUserDetailsService(IMemberRepository iMemberRepository){
-        this.iMemberRepository = iMemberRepository;
-    }
+    @Autowired
+    private IMemberRepository iMemberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
