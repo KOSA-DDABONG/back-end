f1e1f49b67109faa536d58687de0a996a9679dc0
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-08-05 17:56:26 +0900

    [Refactor] Mybati 컨벤션 수정

--- src/main/java/com/ddabong/TripFlow/TripFlowApplication.java
+++ src/main/java/com/ddabong/TripFlow/TripFlowApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ddabong.TripFlow.member.dao")
+@MapperScan("com.ddabong.TripFlow")
 public class TripFlowApplication {
 
 	public static void main(String[] args) {
