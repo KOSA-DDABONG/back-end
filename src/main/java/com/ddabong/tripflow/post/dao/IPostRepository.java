b0a63d24228f43860aca1c4ba5b7a5ea68d87342
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-08-20 21:19:38 +0900

    [Dev] 게시글 작성 기능

---
+++ src/main/java/com/ddabong/tripflow/post/dao/IPostRepository.java
@@ -0,0 +1,14 @@
+package com.ddabong.tripflow.post.dao;
+
+import com.ddabong.tripflow.post.model.Post;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IPostRepository {
+
+    Long getMemberIdByUserId(String userIdByJWT);
+
+    void saveReview(Post post);
+
+    Long getPostIdByTravelId(Long travelId);
+}
