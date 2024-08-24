affe9d29a4a3e6d98f6a35572f159057ba61e668
MG JANG <jang23mg@naver.com>
2024-08-24 20:36:11 +0900

    jwt 토큰 사용변경, 좋아요 저장 및 변경기능 추가

--- src/main/java/com/ddabong/tripflow/board/dao/IBoardRepository.java
+++ src/main/java/com/ddabong/tripflow/board/dao/IBoardRepository.java
@@ -13,30 +13,30 @@ public interface IBoardRepository {
     void updateHits(Long id);
     BoardDTO findById(Long id);
     List<BoardDTO>findLike(Long id);
-    List<BoardDTO>findDetail(Long id);
+    BoardDTO findDetail(Long id);
     void update(BoardDTO boardDTO);
     void delete(Long id);
     //List<BoardDTO>findComment(Long id);
     List<CommentDTO>findComment(Long id);
     List<HashDTO>findHash(Long id);
-
     List<BoardDTO> findTOP();
     void saveComment(CommentDTO commentDTO);
     void savePost(BoardDTO boardDTO);
     void saveImage(ImageDTO imageDTO);
     void savePostImage(PostImageDTO postImageDTO);
     List<ImageDTO>findImage(Long id);
-
     PostImageDTO findPostid();
     Long findImageid();
-
     Long findMemberid(String s);
-
     void saveHash(HashDTO hashDTO);
     void saveHashJoin(HashDTO hashDTO);
     Long findHashid(String s);
-
     Long findLikeCount(Long id);
     Long findCommentCount(Long id);
+    Long findTravelid(Long id);
+    void saveLike(MemberDTO memberDTO);
+    void deleteLike(MemberDTO memberDTO);
+    Boolean findLikeflag(MemberDTO memberDTO);
+    //Long getMemberIdByUserId(String userId);
 
 }
--- src/main/java/com/ddabong/tripflow/board/dto/CommentDTO.java
+++ src/main/java/com/ddabong/tripflow/board/dto/CommentDTO.java
@@ -13,9 +13,11 @@ public class CommentDTO {
     //예를 들어 lombok으로 위와같이 getter setter를 설정했다면
     // getcommentid
     // setcommentid라는 메서드가 이미 존재하고 있다.
+    private String userid;
     private  Long postid;
     private  Long travelid;
     private  Long memberid;
     private  Long commentid2;
     private  String comcontent;
+    private  String createdtime;
 }
